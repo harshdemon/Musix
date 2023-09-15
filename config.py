de6d9a14673bd83e6f8ce680cc23b44d33970d69
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-09-15 06:30:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,11 +83,11 @@ PING_IMG_URL = getenv(
     "https://graph.org/file/ea11748b5ecd674583a99.jpg",
 )
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/ea11748b5ecd674583a99.jpg"
 
-GLOBAL_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+GLOBAL_IMG_URL = "https://graph.org/file/ea11748b5ecd674583a99.jpg"
 
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://graph.org/file/ea11748b5ecd674583a99.jpg"
 
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 
